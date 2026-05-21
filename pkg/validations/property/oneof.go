4c0ac2f88bc9321df75dabd8c769ca4a0b907ea5
Predrag Knezevic <pknezevi@redhat.com>
2026-05-21 14:07:55 +0200

    Add oneOf property validation
    Add validation for the oneOf constraint on CRD properties to detect
    breaking changes: adding a net-new oneOf constraint, removing an
    existing subschema, or adding a new subschema. Includes a configurable
    AdditionPolicy (Allow/Disallow) to control whether adding new
    subschemas is flagged.
    
    Co-authored-by: Vishal P <vishalpalla@gmail.com>

---
+++ pkg/validations/property/oneof.go
@@ -0,0 +1,197 @@
+// Copyright 2025 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package property
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"slices"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/util/sets"
+	"sigs.k8s.io/crdify/pkg/config"
+	"sigs.k8s.io/crdify/pkg/validations"
+)
+
+const oneOfValidationName = "oneOf"
+
+var (
+	_ validations.Validation                                  = (*OneOf)(nil)
+	_ validations.Comparator[apiextensionsv1.JSONSchemaProps] = (*OneOf)(nil)
+)
+
+// RegisterOneOf registers the OneOf validation
+// with the provided validation registry.
+func RegisterOneOf(registry validations.Registry) {
+	registry.Register(oneOfValidationName, oneOfFactory)
+}
+
+// oneOfFactory is a function used to initialize a OneOf validation
+// implementation based on the provided configuration.
+func oneOfFactory(cfg map[string]interface{}) (validations.Validation, error) {
+	oneOfCfg := &OneOfConfig{}
+
+	err := ConfigToType(cfg, oneOfCfg)
+	if err != nil {
+		return nil, fmt.Errorf("parsing config: %w", err)
+	}
+
+	err = ValidateOneOfConfig(oneOfCfg)
+	if err != nil {
+		return nil, fmt.Errorf("validating oneOf config: %w", err)
+	}
+
+	return &OneOf{OneOfConfig: *oneOfCfg}, nil
+}
+
+// ValidateOneOfConfig validates the provided OneOfConfig
+// setting default values where appropriate.
+// Currently the defaulting behavior defaults the
+// OneOfConfig.AdditionPolicy to AdditionPolicyDisallow
+// if it is set to the empty string ("").
+func ValidateOneOfConfig(in *OneOfConfig) error {
+	if in == nil {
+		return nil
+	}
+
+	switch in.AdditionPolicy {
+	case AdditionPolicyAllow, AdditionPolicyDisallow:
+		// do nothing, valid case
+	case AdditionPolicy(""):
+		in.AdditionPolicy = AdditionPolicyDisallow
+	default:
+		return fmt.Errorf("%w : %q", errUnknownAdditionPolicy, in.AdditionPolicy)
+	}
+
+	return nil
+}
+
+// OneOfConfig contains additional configurations for the OneOf validation.
+type OneOfConfig struct {
+	// additionPolicy is how adding subschemas to an existing oneOf
+	// constraint should be treated.
+	// Allowed values are Allow and Disallow.
+	// When set to Allow, adding new subschemas to an existing
+	// oneOf constraint will not be flagged.
+	// When set to Disallow, adding new subschemas to an existing
+	// oneOf constraint will be flagged.
+	// Defaults to Disallow.
+	AdditionPolicy AdditionPolicy `json:"additionPolicy,omitempty"`
+}
+
+// OneOf is a Validation that can be used to identify
+// incompatible changes to the oneOf constraint of CRD properties.
+type OneOf struct {
+	OneOfConfig
+
+	enforcement config.EnforcementPolicy
+}
+
+// Name returns the name of the OneOf validation.
+func (o *OneOf) Name() string {
+	return oneOfValidationName
+}
+
+// SetEnforcement sets the EnforcementPolicy for the OneOf validation.
+func (o *OneOf) SetEnforcement(policy config.EnforcementPolicy) {
+	o.enforcement = policy
+}
+
+// Compare compares an old and a new JSONSchemaProps, checking for incompatible changes to the oneOf constraints of a property.
+// In order for callers to determine if diffs to a JSONSchemaProps have been handled by this validation
+// the JSONSchemaProps.OneOf field will be reset to 'nil' as part of this method.
+// It is highly recommended that only copies of the JSONSchemaProps to compare are provided to this method
+// to prevent unintentional modifications.
+func (o *OneOf) Compare(a, b *apiextensionsv1.JSONSchemaProps) validations.ComparisonResult {
+	oldSchemas := sets.New[string]()
+
+	for i := range a.OneOf {
+		key, err := marshalSchema(a.OneOf[i])
+		if err != nil {
+			a.OneOf = nil
+			b.OneOf = nil
+
+			return validations.HandleErrors(o.Name(), o.enforcement, fmt.Errorf("marshaling old oneOf schema at index %d: %w", i, err))
+		}
+
+		oldSchemas.Insert(key)
+	}
+
+	newSchemas := sets.New[string]()
+
+	for i := range b.OneOf {
+		key, err := marshalSchema(b.OneOf[i])
+		if err != nil {
+			a.OneOf = nil
+			b.OneOf = nil
+
+			return validations.HandleErrors(o.Name(), o.enforcement, fmt.Errorf("marshaling new oneOf schema at index %d: %w", i, err))
+		}
+
+		newSchemas.Insert(key)
+	}
+
+	removedSchemas := oldSchemas.Difference(newSchemas)
+	addedSchemas := newSchemas.Difference(oldSchemas)
+
+	var err error
+
+	switch {
+	case oldSchemas.Len() == 0 && newSchemas.Len() > 0:
+		newSchemaSlice := newSchemas.UnsortedList()
+		slices.Sort(newSchemaSlice)
+		err = fmt.Errorf("%w: %v", ErrNetNewOneOfConstraint, newSchemaSlice)
+	case removedSchemas.Len() > 0:
+		removedSchemaSlice := removedSchemas.UnsortedList()
+		slices.Sort(removedSchemaSlice)
+		err = fmt.Errorf("%w: %v", ErrRemovedOneOf, removedSchemaSlice)
+	case addedSchemas.Len() > 0 && o.AdditionPolicy != AdditionPolicyAllow:
+		addedSchemaSlice := addedSchemas.UnsortedList()
+		slices.Sort(addedSchemaSlice)
+		err = fmt.Errorf("%w: %v", ErrAddedOneOf, addedSchemaSlice)
+	}
+
+	a.OneOf = nil
+	b.OneOf = nil
+
+	return validations.HandleErrors(o.Name(), o.enforcement, err)
+}
+
+// marshalSchema produces a canonical JSON string for a schema,
+// ignoring non-structural fields (Description, Example) so that
+// only validation-relevant differences are compared.
+func marshalSchema(schema apiextensionsv1.JSONSchemaProps) (string, error) {
+	schema.Description = ""
+	schema.Example = nil
+
+	data, err := json.Marshal(schema)
+	if err != nil {
+		return "", fmt.Errorf("marshaling schema: %w", err)
+	}
+
+	return string(data), nil
+}
+
+var (
+	// ErrNetNewOneOfConstraint represents an error state where a net new oneOf constraint was added to a property.
+	ErrNetNewOneOfConstraint = errors.New("oneOf constraint added when there was none previously")
+	// ErrRemovedOneOf represents an error state where at least one previously allowed oneOf subschema was removed
+	// from the oneOf constraint on a property.
+	ErrRemovedOneOf = errors.New("allowed oneOf schemas removed")
+	// ErrAddedOneOf represents an error state where at least one oneOf subschema, that was not previously allowed,
+	// was added to the oneOf constraint on a property.
+	ErrAddedOneOf = errors.New("allowed oneOf schemas added")
+)
---
+++ pkg/validations/property/oneof_test.go
@@ -0,0 +1,224 @@
+// Copyright 2025 The Kubernetes Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package property
+
+import (
+	"errors"
+	"testing"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	internaltesting "sigs.k8s.io/crdify/pkg/validations/internal/testing"
+)
+
+func TestOneOf(t *testing.T) {
+	testcases := []internaltesting.Testcase[apiextensionsv1.JSONSchemaProps]{
+		{
+			Name: "no diff, not flagged",
+			Old: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+				},
+			},
+			New: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+				},
+			},
+			Flagged:              false,
+			ComparableValidation: &OneOf{},
+		},
+		{
+			Name: "no diff different order, not flagged",
+			Old: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+					{Type: "string"},
+				},
+			},
+			New: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "string"},
+					{Type: "integer"},
+				},
+			},
+			Flagged:              false,
+			ComparableValidation: &OneOf{},
+		},
+		{
+			Name: "new oneOf constraint, flagged",
+			Old:  &apiextensionsv1.JSONSchemaProps{},
+			New: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+				},
+			},
+			Flagged:              true,
+			ComparableValidation: &OneOf{},
+		},
+		{
+			Name: "removed oneOf subschema, flagged",
+			Old: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+					{Type: "string"},
+				},
+			},
+			New: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "string"},
+				},
+			},
+			Flagged:              true,
+			ComparableValidation: &OneOf{},
+		},
+		{
+			Name: "new allowed oneOf subschema added, addition policy not set, flagged",
+			Old: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+				},
+			},
+			New: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+					{Type: "string"},
+				},
+			},
+			Flagged:              true,
+			ComparableValidation: &OneOf{},
+		},
+		{
+			Name: "new allowed oneOf subschema added, addition policy set to Disallow, flagged",
+			Old: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+				},
+			},
+			New: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+					{Type: "string"},
+				},
+			},
+			Flagged: true,
+			ComparableValidation: &OneOf{
+				OneOfConfig: OneOfConfig{
+					AdditionPolicy: AdditionPolicyDisallow,
+				},
+			},
+		},
+		{
+			Name: "new allowed oneOf subschema added, addition policy set to Allow, not flagged",
+			Old: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+				},
+			},
+			New: &apiextensionsv1.JSONSchemaProps{
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+					{Type: "string"},
+				},
+			},
+			Flagged: false,
+			ComparableValidation: &OneOf{
+				OneOfConfig: OneOfConfig{
+					AdditionPolicy: AdditionPolicyAllow,
+				},
+			},
+		},
+		{
+			Name: "different field changed, not flagged",
+			Old: &apiextensionsv1.JSONSchemaProps{
+				ID: "foo",
+			},
+			New: &apiextensionsv1.JSONSchemaProps{
+				ID: "bar",
+			},
+			Flagged:              false,
+			ComparableValidation: &OneOf{},
+		},
+		{
+			Name: "different field changed with oneOf, not flagged",
+			Old: &apiextensionsv1.JSONSchemaProps{
+				ID: "foo",
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+				},
+			},
+			New: &apiextensionsv1.JSONSchemaProps{
+				ID: "bar",
+				OneOf: []apiextensionsv1.JSONSchemaProps{
+					{Type: "integer"},
+				},
+			},
+			Flagged:              false,
+			ComparableValidation: &OneOf{},
+		},
+	}
+
+	internaltesting.RunTestcases(t, testcases...)
+}
+
+func TestValidateOneOfConfig(t *testing.T) {
+	testcases := []struct {
+		name               string
+		cfg                *OneOfConfig
+		wantErr            error
+		wantAdditionPolicy AdditionPolicy
+	}{
+		{
+			name: "nil config",
+			cfg:  nil,
+		},
+		{
+			name:               "defaults addition policy",
+			cfg:                &OneOfConfig{},
+			wantAdditionPolicy: AdditionPolicyDisallow,
+		},
+		{
+			name:               "allows valid addition policies",
+			cfg:                &OneOfConfig{AdditionPolicy: AdditionPolicyAllow},
+			wantAdditionPolicy: AdditionPolicyAllow,
+		},
+		{
+			name:    "invalid addition policy",
+			cfg:     &OneOfConfig{AdditionPolicy: "invalid"},
+			wantErr: errUnknownAdditionPolicy,
+		},
+	}
+
+	for _, tc := range testcases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			err := ValidateOneOfConfig(tc.cfg)
+			if tc.wantErr != nil {
+				if !errors.Is(err, tc.wantErr) {
+					t.Fatalf("expected error %v, got %v", tc.wantErr, err)
+				}
+
+				return
+			}
+
+			if err != nil {
+				t.Fatalf("unexpected error: %v", err)
+			}
+
+			if tc.cfg != nil && tc.cfg.AdditionPolicy != tc.wantAdditionPolicy {
+				t.Fatalf("expected addition policy %q, got %q", tc.wantAdditionPolicy, tc.cfg.AdditionPolicy)
+			}
+		})
+	}
+}
